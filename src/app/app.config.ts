e1b34cf1da8cfd750260b5ba547706282c222a17
anon <anon@example.com>
2025-07-23 12:55:08 +0100

    add: environments and wire up backend apiUrl

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { API_URL } from './features/task/task-api.token';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    // inject api from environment
+    { provide: API_URL, useValue: environment.apiUrl }
   ]
 };
