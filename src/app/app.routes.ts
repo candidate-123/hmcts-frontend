f446101ed4e6cb08639573df1211bdd9970d75c3
anon <anon@example.com>
2025-07-24 13:51:15 +0100

    add: delete task desigfjn and func.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ export const routes: Routes = [
   { path: '', redirectTo: 'tasks', pathMatch: 'full' },
   { path: 'tasks', loadComponent: () => import("./features/task/pages/tasks").then(m => m.Tasks) },
   { path: 'tasks/:id/edit', loadComponent: () => import("./features/task/pages/task-edit/task-edit").then(m => m.TaskEdit) },
-
+  { path: 'tasks/:id/deleteTask', loadComponent: () => import("./features/task/pages/task-delete/task-delete").then(m => m.TaskDelete) },
   { path: 'error', loadComponent: () => import("./core/pages/error/error").then(m => m.Error) }
 
 ];
---
+++ src/app/features/task/pages/task-delete/task-delete.ts
@@ -0,0 +1,42 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { TaskApi } from '../../task-api';
+
+
+@Component({
+  selector: 'app-task-delete',
+  imports: [RouterModule],
+  template: `
+  <h1 class="govuk-heading-l">Delete {{ type }}?</h1>
+<p class="govuk-body">Are you sure you want to delete this {{ type }}? This action cannot be undone.</p>
+
+<div class="govuk-button-group">
+  <button class="govuk-button govuk-button--warning" (click)="deleteTask()" [disabled]="isLoading">
+    {{ isLoading ? 'Deleting...' : 'Yes, delete' }}
+  </button>
+  <a routerLink="/{{ type }}s/{{ id }}" class="govuk-link">Cancel</a>
+</div>
+  `,
+  styles: ``
+})
+export class TaskDelete implements OnInit {
+
+  id!: number;
+  isLoading = false;
+  type = 'task';
+  taskService = inject(TaskApi);
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    const qp = this.route.snapshot.queryParamMap;
+    this.id = +qp.get('id')!;
+  }
+
+  deleteTask() {
+    this.taskService.deleteTask(this.id).subscribe({
+      next: () => this.router.navigate([`/${this.type.toLowerCase()}s`]),
+
+    });
+  }
+}
